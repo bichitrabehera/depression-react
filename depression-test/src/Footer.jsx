eb4f05a47442d84cd343b022c2cf43ab74ccf8aa
bichitrabehera <bichitrabehera.345@gmail.com>
2024-12-12 18:47:02 +0530

    first commit

---
+++ depression-test/src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+    return(
+        <footer>
+            <p>We care - 2024</p>
+        </footer>
+    );
+}
+
+export default Footer
---
+++ depression-test/src/Header.jsx
@@ -0,0 +1,13 @@
+function Header(){
+    return(
+        <header>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </header>
+    );
+}
+
+export default Header
