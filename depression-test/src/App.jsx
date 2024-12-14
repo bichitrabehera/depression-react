f462dec7d40197f30fc1bcd3b6a03fd80a485f58
bichitrabehera <bichitrabehera.345@gmail.com>
2024-12-14 07:29:01 +0530

    added first page

--- depression-test/src/App.jsx
+++ depression-test/src/App.jsx
@@ -1,13 +1,38 @@
-import Header from './Header.jsx'
-import Footer from './Footer.jsx'
-
+import React from 'react';
+import './index.css';
 
 function App() {
-    return(
-        <>
-        <Header></Header>
-        <Footer></Footer>
-        </>
+    return (
+        <div className="container">
+            <nav className="navbar">
+                <ul className="navLinks">
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Therapists</a></li>
+                    <li><a href="">Reviews</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+                <button className="loginButton">Login</button>
+            </nav>
+            <div className="content">
+                <div className="leftSection">
+                    <p className="text">
+                        It is normal to feel <br />
+                        sad, stressed and <br />
+                        anxious during a crisis
+                    </p>
+                    <button className="therapistButton">Talk with a therapist</button>
+                </div>
+                <div className="rightSection">
+                    <div className="eye">
+                        <div className="eyeBall"></div>
+                        <div className="tear"></div>
+                    </div>
+                </div>
+            </div>
+            <footer className="footer">
+                <span>Talking to people you trust can help</span>
+            </footer>
+        </div>
     );
 }
 
--- depression-test/src/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer(){
-    return(
-        <footer>
-            <p>We care - 2024</p>
-        </footer>
-    );
-}
-
-export default Footer
